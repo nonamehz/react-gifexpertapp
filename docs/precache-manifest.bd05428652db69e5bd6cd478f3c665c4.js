d9dbf5b4e7a4afb690c297211827ec5d34155b12
Jose Caballero <acg_1349@hotmail.com>
2020-11-17 01:39:49 -0500

    docs agregado

---
+++ docs/precache-manifest.bd05428652db69e5bd6cd478f3c665c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec9c7d200773f0ddec69b5705d25c45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a7a15bc7c32d169374f",
+    "url": "/static/css/main.e13a6c2e.chunk.css"
+  },
+  {
+    "revision": "a13e31d6699d5138f112",
+    "url": "/static/js/2.bed60b36.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.bed60b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a7a15bc7c32d169374f",
+    "url": "/static/js/main.5b6a5053.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
